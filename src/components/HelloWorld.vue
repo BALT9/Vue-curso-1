61b3525be393f051b5c2013cd8234c0810bfab6f
willy <willybalt.2017@gmail.com>
2026-03-21 20:34:56 -0400

    uso de reactive, ref

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,17 +1,22 @@
 <script setup>
-import { ref } from "vue";
+import { reactive, ref } from "vue";
 
 const count = ref(0);
-const name = ref("")
-const lastName = ref("")
-const age = ref(0)
+const name = "willy";
+const lastName = "chura";
+const age = 22;
 
-const user = ref({
+// reactive para enviar sin user.value (tambien se puede usar ref pero se debe acceder con user.value)
+const user = reactive({
   name: name,
   lastName: lastName,
-  age: age
-})
+  age: age,
+});
 
+function enviarDatos() {
+  alert("Datos enviados");
+  console.log(JSON.stringify(user, null, 2));
+}
 </script>
 
 <template>
@@ -19,15 +24,23 @@ const user = ref({
   <button v-on:click="count++">Numero {{ count }}</button>
 
   <h1>Tu nombre</h1>
-  <input type="text" v-model="name" placeholder="Ingresa tu nombre">
+  <input type="text" v-model="user.name" placeholder="Ingresa tu nombre" />
   <h1>Tu apellido</h1>
-  <input type="text" v-model="lastName" placeholder="Ingresa tu apellido">
+  <input
+    type="text"
+    v-model="user.lastName"
+    placeholder="Ingresa tu apellido"
+  />
   <h1>Tu edad</h1>
-  <input type="text" v-model="age" placeholder="Ingresa tu edad">
-  <p>Bienvenido {{ name }} {{ lastName }}, tu edad es {{ age }} años</p>
+  <input type="text" v-model="user.age" placeholder="Ingresa tu edad" />
+  <p>
+    Bienvenido {{ user.name }} {{ user.lastName }}, tu edad es
+    {{ user.age }} años
+  </p>
 
   <h2>Objeto User</h2>
   <!-- <p>{{ user }}</p> -->
   <pre>{{ JSON.stringify(user, null, 2) }}</pre>
 
+  <button v-on:click="enviarDatos">Enviar Datos</button>
 </template>
