71998752cd20ed938058f6911b1a8f19f812fd55
willy <willybalt.2017@gmail.com>
2026-03-21 20:02:20 -0400

    creacion del proyecto

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,10 @@
+<script setup>
+import { ref } from 'vue'
+
+const count = ref(0)
+</script>
+
+<template>
+  <h1>Bienvenido a Vue</h1>
+  <button v-on:click="count++">Numero {{ count }}</button>
+</template>
