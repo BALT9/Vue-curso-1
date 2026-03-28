bf53092af52189c186f17b392e066d59d65391cd
willy <willybalt.2017@gmail.com>
2026-03-28 19:28:50 -0400

    nuevo ajuste de deploy

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,10 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/Vue-curso-1',
+  build: {
+    outDir: 'dist'
+  },
   plugins: [vue()],
   outputDir: 'Vue-curso-1'
 })
