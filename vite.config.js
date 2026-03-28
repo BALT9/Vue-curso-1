5287e9a21fd584abbf06d72dc431e7ac6e875e36
willy <willybalt.2017@gmail.com>
2026-03-28 19:33:50 -0400

    solucion 404 github pages

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,10 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/Vue-curso-1',
+  base: '/Vue-curso-1/', // 👈 IMPORTANTE
+  plugins: [vue()],
   build: {
     outDir: 'dist'
-  },
-  plugins: [vue()],
-  outputDir: 'Vue-curso-1'
+  }
 })
