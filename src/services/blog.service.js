f5ef18c829273dc5fb4794c579baada52b1cdefb
willy <willybalt.2017@gmail.com>
2026-03-29 14:46:49 -0400

    axios , services

--- src/services/blog.service.js
+++ src/services/blog.service.js
@@ -1,33 +1,33 @@
-import axios from "axios";
+import { api } from "./api";
 
 export default {
     async obtenerPublicacion() {
-        const { data } = await axios.get('https://dev.to/api/articles');
+        const { data } = await api().get('/articles');
         console.log("Datos para consola", data)
         return data;
     },
 
     async guardarPublicacion(datos) {
-        const { data } = await axios.post('https://dev.to/api/articles', datos);
+        const { data } = await api().post('/articles', datos);
         console.log("Datos para consola", data)
         return data;
     },
 
     async obtenerPublicacionID(id) {
-        const { data } = await axios.get('https://dev.to/api/articles' + id);
+        const { data } = await api().get('/articles' + id);
         console.log("Datos para consola", data)
         return data;
     },
 
     async actualizarPublicacion(id, datos) {
-        const { data } = await axios.put('https://dev.to/api/articles' + id, datos);
+        const { data } = await api().put('/articles' + id, datos);
         console.log("Datos para consola", data)
         return data;
     },
 
     async eliminarPublicacion(id) {
-        const { data } = await axios.delete('https://dev.to/api/articles' + id);
+        const { data } = await api().delete('/articles' + id);
         console.log("Datos para consola", data)
         return data;
     },
-}
+} 
