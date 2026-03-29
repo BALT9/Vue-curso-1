348fe86a366f552d19fa70d638654a1466704640
willy <willybalt.2017@gmail.com>
2026-03-29 14:24:43 -0400

    ej conexion a enpoints

--- src/services/blog.service.js
+++ src/services/blog.service.js
@@ -1,15 +1,33 @@
 import axios from "axios";
 
 export default {
-    async obtenerPublicacion(){
-        const {data} = await axios.get('https://dev.to/api/articles');
-        console.log("Datos para consola",data)
+    async obtenerPublicacion() {
+        const { data } = await axios.get('https://dev.to/api/articles');
+        console.log("Datos para consola", data)
         return data;
-    } ,
+    },
 
-    async guardarPublicacion(){
-        const {data} = await axios.get('https://dev.to/api/articles');
-        console.log("Datos para consola",data)
+    async guardarPublicacion(datos) {
+        const { data } = await axios.post('https://dev.to/api/articles', datos);
+        console.log("Datos para consola", data)
         return data;
-    } 
+    },
+
+    async obtenerPublicacionID(id) {
+        const { data } = await axios.get('https://dev.to/api/articles' + id);
+        console.log("Datos para consola", data)
+        return data;
+    },
+
+    async actualizarPublicacion(id, datos) {
+        const { data } = await axios.put('https://dev.to/api/articles' + id, datos);
+        console.log("Datos para consola", data)
+        return data;
+    },
+
+    async eliminarPublicacion(id) {
+        const { data } = await axios.delete('https://dev.to/api/articles' + id);
+        console.log("Datos para consola", data)
+        return data;
+    },
 }
